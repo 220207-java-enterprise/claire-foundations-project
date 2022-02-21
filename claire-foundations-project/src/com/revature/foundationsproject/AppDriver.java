bfa17ba51b16fe2ed76ef8609299c57f0a7438b0
Claire Abel <claire.m.abel@gmail.com>
2022-02-21 14:24:28 -0600

    first package and class added

---
+++ claire-foundations-project/src/com/revature/foundationsproject/AppDriver.java
@@ -0,0 +1,8 @@
+package com.revature.foundationsproject;
+
+public class AppDriver {
+
+    public static void main(String[] args){
+        System.out.println("Beginning of Foundations Project.");
+    }
+}
